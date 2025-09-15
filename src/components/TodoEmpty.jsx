9628ee59cf2058d1eaa782c9355e540ea5bb367a
ValentinFM <127439244+ValentinFM@users.noreply.github.com>
2025-09-15 11:09:07 -0300

    Delete src/components directory

--- src/components/TodoEmpty.jsx
+++
@@ -1,7 +0,0 @@
-function TodoEmpty() {
-  return (
-    <p className="empty-message">No hay tareas en la lista 📝</p>
-  );
-}
-
-export default TodoEmpty;
--- src/components/TodoHeader.jsx
+++
@@ -1,5 +0,0 @@
-function TodoHeader() {
-    return <h1>LISTA DE TAREAS DE VALENTIN</h1>;
-}
-
-export default TodoHeader;
